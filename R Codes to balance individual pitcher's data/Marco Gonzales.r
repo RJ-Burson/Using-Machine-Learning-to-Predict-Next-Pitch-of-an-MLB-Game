85ee8f183fc97c96525cbeed675b97c8936b5003
RJ-Burson <140913316+RJ-Burson@users.noreply.github.com>
2024-07-24 19:11:20 -0700

    Add files via upload

--- R Codes to balance individual pitcher's data/Marco Gonzales.r
+++ R Codes to balance individual pitcher's data/Marco Gonzales.r
@@ -12,11 +12,14 @@ ff_cut= subset(train_set, pitch_name==1 | pitch_name==4)
 
 
 
-BalancedData1 = ovun.sample(pitch_name~. , ff_ch, method="over", p=0.75, subset=options("subset")$subset, na.action=options("na.action")$na.action, seed)$data
+BalancedData1 = ovun.sample(pitch_name~. , ff_ch, method="over", p=0.75, subset=options("subset")$subset,
+                            na.action=options("na.action")$na.action, seed)$data
 
-BalancedData2 = ovun.sample(pitch_name~. , ff_cu, method="over", p=0.75, subset=options("subset")$subset, na.action=options("na.action")$na.action, seed)$data
+BalancedData2 = ovun.sample(pitch_name~. , ff_cu, method="over", p=0.75, subset=options("subset")$subset,
+                            na.action=options("na.action")$na.action, seed)$data
 
-BalancedData3 = ovun.sample(pitch_name~. , ff_cut, method="over", p=0.75, subset=options("subset")$subset, na.action=options("na.action")$na.action, seed)$data
+BalancedData3 = ovun.sample(pitch_name~. , ff_cut, method="over", p=0.75, subset=options("subset")$subset,
+                            na.action=options("na.action")$na.action, seed)$data
 
 
 ## Combining all of the pitches into one balanced dataset
@@ -28,12 +31,14 @@ train_set = na.omit(train_set)
 validation_set = na.omit(validation_set)
 final_test = na.omit(final_test)
 
+
 train_set$pitch_name <- factor(train_set$pitch_name, 
                                levels=c(1,2,3,4,5,6,7,8) )
 validation_set$pitch_name <- factor(validation_set$pitch_name, 
                                     levels=c(1,2,3,4,5,6,7,8) )
 final_test$pitch_name <- factor(final_test$pitch_name, 
                                 levels=c(1,2,3,4,5,6,7,8) )
+
 
 train_set = droplevels.data.frame(train_set)
 validation_set = droplevels.data.frame(validation_set)
